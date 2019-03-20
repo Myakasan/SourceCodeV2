3494774a09029261db81c339d70fc45fd9819020
Etienne Mya K <egendron6@icloud.com>
2019-03-20 11:40:31 +0100

    set up view

---
+++ sourcecode/resources/views/echec.blade.php
@@ -0,0 +1,5 @@
+@extends('template')
+
+@section('content')
+<h1>Ce n'est pas comme ça que vous allez retrouver Théo</h1>
+@endsection
---
+++ sourcecode/resources/views/success.blade.php
@@ -0,0 +1,9 @@
+@extends('templates')
+
+@section('content')
+
+<h1>Bravo vous avez craqué le systeme</h1>
+
+<p>la suite de l'enigme c'est...</p>
+
+@endsection
