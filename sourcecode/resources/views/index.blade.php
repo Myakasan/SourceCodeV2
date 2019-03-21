9382a3cf091190c76deb1fbf27486fc7618aef3b
Etienne Mya K <egendron6@icloud.com>
2019-03-21 13:00:43 +0100

    Add Style

--- sourcecode/resources/views/index.blade.php
+++ sourcecode/resources/views/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 
-<h2>Indice 1</h2> <span id="q1" class="badge badge-success">Success</span>
+<h2>Code 1 <span id="q1" class="badge badge-success"><i class="fas fa-check"></i></span> </h2>
 <form action="{{ url('quizbla') }}" method="post">
     {{ csrf_field() }}
 
@@ -12,46 +12,48 @@
                 <input type="text" id="q1" name="q1" required
                        minlength="0" maxlength="50" size="50"  placeholder="<p>hello world</p>">
             </div>
-        </div>
+        </div> <br>
 
-            <h2>Indice 2</h2><span id="q2" class="badge badge-success">Success</span>
+            <h2>Code 2 <span id="q2" class="badge badge-success"><i class="fas fa-check"></i></span> </h2>
             <div class="row">
                 <div class="col">
                     <input type="text" id="q2" name="q2" required
                            minlength="0" maxlength="50" size="50" placeholder="console.log('hello world');">
                 </div>
-            </div>
+            </div> <br>
 
-        <h2>Indice 3</h2><span id="q3" class="badge badge-success">Success</span>
+        <h2>Code 3 <span id="q3" class="badge badge-success"><i class="fas fa-check"></i></span> </h2>
         <div class="row">
             <div class="col">
                 <input type="text" id="q3" name="q3" required
                        minlength="0" maxlength="50" size="50" placeholder="echo 'hello world';">
             </div>
-        </div>
+        </div> <br>
 
-          <h2>Indice 4</h2><span id="q4" class="badge badge-success">Success</span>
+          <h2>Code 4 <span id="q4" class="badge badge-success"><i class="fas fa-check"></i></span></h2>
           <div class="row">
               <div class="col">
                   <input type="text" id="q4" name="q4" required
                          minlength="0" maxlength="50" size="50" placeholder="print('hello world')">
               </div>
-          </div>
+          </div> <br>
 
-            <h2>Indice 5</h2><span id="q4" class="badge badge-success">Success</span>
+            <h2>Code 5 <span id="q4" class="badge badge-success"><i class="fas fa-check"></i></span></h2>
             <div class="row">
                 <div class="col">
                     <input type="text" id="q5" name="q5" required
                            minlength="0" maxlength="50" size="50" placeholder=" c'est trop long a écrire en Django ">
                 </div>
-            </div>
+            </div> <br>
 
-              <div class="row">
+          <br><div class="row">
                 <div class="col">
-                  <button type="submit" class="btn btn-light">Valider</button>
+                  <button type="submit" class="btn btn-light"><i class="fas fa-laptop-code"></i></button>
                 </div>
               </div>
-</form>
+</form> <br>
+
+
 
 
 @endsection
