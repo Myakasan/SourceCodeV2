714b5cb56d0a083d8e82aa28fab6fc8d099da798
Vacance <kenny.vacance@telecom-st-etienne.fr>
2019-03-21 19:18:57 +0100

    Ouf ...

--- sourcecode/app/Http/Controllers/quizcontroller.php
+++ sourcecode/app/Http/Controllers/quizcontroller.php
@@ -95,13 +95,14 @@ class quizcontroller extends Controller
 
         else {
 
+//            $lose = rand(1,3);
             $quizRep1 = $quizRep["q1"];
             $quizRep2 = $quizRep["q2"];
             $quizRep3 = $quizRep["q3"];
             $quizRep4 = $quizRep["q4"];
             $quizRep5 = $quizRep["q5"];
 
-            return view('echec', compact('badge1','badge2','badge3','badge4','badge5', 'quizRep1', 'quizRep2', 'quizRep3', 'quizRep4', 'quizRep5' ));
+            return view("index", compact('badge1','badge2','badge3','badge4','badge5', 'quizRep1', 'quizRep2', 'quizRep3', 'quizRep4', 'quizRep5' ));
         }
 
     }
